1ad3a921fd4b4d804b95e06baa9f361ff92310c3
Hektor-Augusto <hektoraugusto.s@gmail.com>
2024-04-23 18:50:40 -0300

    feat(changes to the register)

--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,9 +1,30 @@
 import './Register.css';
 
+import { useState } from 'react';
+
 const Register = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     const handleRegister = async (e) => {
         e.preventDefault();
+
+        const user = {
+            name,
+            email,
+            password,
+            confirmPassword
+        }
+
+        console.log("User:", user)
+
+        setName("");
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
     };
 
     return (
@@ -13,35 +34,47 @@ const Register = () => {
                     <h1 className='mb-5 text-center'>Registrar o Usuário:</h1>
                     <form onSubmit={handleRegister}>
                         <div className="mb-3 d-flex flex-column justify-content-center align-items-center">
-                            <div className="container-register mb-5">
+                            <div className={`container-register mb-5 ${name ? 'has-content' : ''}`}>
                                 <input
                                     type="text"
                                     name="name"
                                     className="input-register"
+                                    value={name || ""}
+                                    onChange={(e) => setName(e.target.value)}
+                                    required
                                 />
                                 <label className="label-register">Nome Completo</label>
                             </div>
-                            <div className="container-register mb-5">
+                            <div className={`container-register mb-5 ${email ? 'has-content' : ''}`}>
                                 <input
                                     type="email"
                                     name="email"
                                     className="input-register"
+                                    value={email || ""}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
                                 />
                                 <label className="label-register">E-mail</label>
                             </div>
-                            <div className="container-register mb-5">
+                            <div className={`container-register mb-5 ${password ? 'has-content' : ''}`}>
                                 <input
                                     type="password"
                                     name="password"
                                     className="input-register"
+                                    value={password || ""}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
                                 />
                                 <label className="label-register">Senha</label>
                             </div>
-                            <div className="container-register mb-5">
+                            <div className={`container-register mb-5 ${confirmPassword ? 'has-content' : ''}`}>
                                 <input
                                     type="password"
                                     name="confirmPassowrd"
                                     className="input-register"
+                                    value={confirmPassword || ""}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                    required
                                 />
                                 <label className="label-register">Confirme a Senha</label>
                             </div>
