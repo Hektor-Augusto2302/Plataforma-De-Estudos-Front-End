f6ea0e172dd816f5ae4661ac9134f85b4c83bf3a
Hektor-Augusto <hektoraugusto.s@gmail.com>
2024-04-29 13:06:29 -0300

    feat(admin navbar)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,16 @@
 import './Navbar.css';
-
 import { NavLink } from 'react-router-dom';
-import { Context } from '../../context/UserContext';
 import { useContext } from 'react';
+import { Context } from '../../context/UserContext';
 
 const Navbar = () => {
-
     const { user, logout } = useContext(Context);
 
     return (
         <nav className="d-flex justify-content-between align-items-center navbar navbar-expand-sm">
             <div className="container">
-                <NavLink to='/'>
-                    <h3 className='brand navbar-brand'>Plataforma de Estudos</h3>
+                <NavLink to="/">
+                    <h3 className="brand navbar-brand">Plataforma de Estudos</h3>
                 </NavLink>
                 <div className="d-flex flex-column justify-content-end">
                     <button
@@ -26,42 +24,36 @@ const Navbar = () => {
                     >
                         <i className="bi bi-text-left icon-hamburguer"></i>
                     </button>
-                    <ul className="navbar-nav collapse navbar-collapse list-unstyled" id='navbarNav'>
-                        {user ?
-                            (
-                                <>
-                                    <li className='links nav-item me-3 mb-1'>
-                                        <NavLink to='/'>
-                                            Home
-                                        </NavLink>
+                    <ul className="navbar-nav collapse navbar-collapse list-unstyled" id="navbarNav">
+                        {user ? (
+                            <>
+                                <li className="links nav-item me-3 mb-1">
+                                    <NavLink to="/">Home</NavLink>
+                                </li>
+                                {user.role === 'admin' && (
+                                    <li className="links nav-item me-3 mb-1">
+                                        <NavLink to="/admin/registrar">Registrar Admin</NavLink>
                                     </li>
-                                    <li className='links nav-item me-3 mb-1'>
-                                        <button onClick={logout}>
-                                            Sair
-                                        </button>
-                                    </li>
-                                </>
-                            ) :
-                            (
-                                <>
-                                    <li className='links nav-item me-3 mb-1'>
-                                        <NavLink to='/entrar'>
-                                            Entrar
-                                        </NavLink>
-                                    </li>
-                                    <li className='links nav-item me-3 mb-1'>
-                                        <NavLink to='/registrar'>
-                                            Registrar
-                                        </NavLink>
-                                    </li>
-                                </>
-                            )
-                        }
+                                )}
+                                <li className="links nav-item me-3 mb-1">
+                                    <button onClick={logout}>Sair</button>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className="links nav-item me-3 mb-1">
+                                    <NavLink to="/entrar">Entrar</NavLink>
+                                </li>
+                                <li className="links nav-item me-3 mb-1">
+                                    <NavLink to="/registrar">Registrar</NavLink>
+                                </li>
+                            </>
+                        )}
                     </ul>
                 </div>
             </div>
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
