4574e90d82582694ba224bc1ae20574761f72c9e
Hektor-Augusto <hektoraugusto.s@gmail.com>
2024-05-06 12:10:11 -0300

    feat(updating the page)

--- src/pages/Questions/Questions.jsx
+++ src/pages/Questions/Questions.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import './Questions.css';
 
 const Questions = () => {
     return (
-        <div>Questions</div>
-    )
-}
+        <div className="container">
+            <div className="row justify-content-center align-items-center">
+                <div className="col-12 col-md-10">
+                    <form className='mt-5 title-history d-flex flex-column flex-md-row justify-content-center align-items-center'> {/* Flex para responsividade */}
+                        <h3 className='me-3 text-center'>Escolha o período histórico para fazer o simulado:</h3>
+                        <select className="form-select form-width-select" aria-label="Seleção de período">
+                            <option value="todos" selected>Todos</option>
+                            <option value="one">Um</option>
+                            <option value="two">Dois</option>
+                            <option value="three">Três</option>
+                        </select>
+                    </form>
+                </div>
+                <div className="col-12 col-md-10 mt-5">
+                    <h1>Teste</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Questions
+export default Questions;
